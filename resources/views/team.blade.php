5f99237bccc9cadd6ec98c49f49b439b37bb43c8
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-25 16:57:30 +0200

    hello world stamped

---
+++ resources/views/team.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team</title>
+</head>
+<body>
+    <header>
+        <div class="text-center">
+            <h1 class="text-primary">Membri del Team</h1>
+        </div>
+        
+    </header>
+
+    <main>
+        <div>
+            
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+    
+</body>
+</html>
